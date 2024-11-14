0b4cacbdbdceddf3aa12673e819c4366f0501a5b
NightmareKNG <phiradatha11@gmail.com>
2024-11-14 20:05:24 +0700

    ข้อความอธิบายการเปลี่ยนแปลง

--- configfirebase.js
+++ configfirebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
-
+const serviceAccount = JSON.parse(process.env.GOOGLE_CLOUD_KEY);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: "project-adw.appspot.com"
