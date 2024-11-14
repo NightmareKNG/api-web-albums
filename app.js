99680de86fb1d3dc60b8ddc82616714ec5c669f2
NightmareKNG <phiradatha11@gmail.com>
2024-11-14 21:02:05 +0700

    Add serviceAccountKey.json to .gitignore3

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const albums_type = require('./routes/albums_type');
 require('dotenv').config()
 const url = process.env.MONGO_URI
 const cors = require('cors');
+app.use(cors());
 mongoose.Promise = global.Promise;
 
 mongoose.connect(url)
@@ -29,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
