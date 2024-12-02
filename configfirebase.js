9f13b4ca82a88ab2a6f77f178c440cb6d84b355e
NightmareKNG <phiradatha11@gmail.com>
2024-12-02 11:33:37 +0700

    Initial commit001

--- configfirebase.js
+++ configfirebase.js
@@ -1,11 +1,8 @@
 require("dotenv").config();
 const admin = require("firebase-admin");
 
-const fs = require("fs");
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync("serviceAccountKey.json", "utf8")
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: "project-adw.appspot.com"
